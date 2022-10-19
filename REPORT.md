5ec294c50d4bce780d6152876a7a4831630b6001
PizzaDeliveryOP <114107987+PizzaDeliveryOP@users.noreply.github.com>
2022-10-19 17:25:08 -0400

    Add files via upload

--- REPORT.md
+++ REPORT.md
@@ -9,22 +9,37 @@ Your tone should be professional but informal: imagine that you are responding t
 ---
 
 1. Who was the top salesperson for the year?
-asd
+Dirk T. Stringer
 
 2. How much better did the top salesperson do than the individual who came in second for the year?
+Mr. Stringer made about 40% more sales than the next best salesman, 
+$61,022,268.45 for Stringer vs $36,413,772.33 for the next best; Defrance.
+
 
 
 3. Was there any relationship between the average sale amount for the top salespersons and the total sales?
-
+There is a limited relationship, it appears that Albert (third highest salesman) focused on selling many items across multiple sales events. Michel (second highest performing salesman) did not have as many sales events, but sold higher priced items. Dirk (highest performing salesman)
 
 4. How do you think the top selling salesperson gained so many sales? What was their strategy?
 
 - Did they sell to many customers, or a dedicated few?
 - Did they tend to sell a few big ticket (high priced) items, or did they focus on a high volume of lower priced items?
 
+They tended to focus on selling fewer higher priced items, but in respectably high volumes, and to essentially one dedicated customer. 
+
  
 
 5. What was the top selling product for the year? What was the lowest selling? (For both dollar amount sold and quantity sold).
 
+The Top Selling item in raw sales was 'Road-350-W Yellow, 42' which brought in $16,720,731.70 in sales. 
+The Lowest Selling item in raw sales was a tie between External Lock Washer 7,Flat Washer 4,Chainring Bolts, Lock Washer 3, External Lock Washer 2, which all brought in $0 each. Is this correct? 
+The Lowest Selling Item in raw sales that brought in actual sales was 'Adjustable Race' whcih brought in $849.60 in sales.  
+
+The Top Selling item in Quantity was 'Touring-3000 Blue, 54' which sold 24,175 units.
+The Lowest Selling item in Quantity was 'Chain Stays' which only sold 169 units. 
 
 6. What problems did you find in the data? (For example, where there any products without a price?)
+There were several products without prices. Is it common practice to be giving these items away? The data suggests sometimes hundreds of say, Chainring Bolts, are 'sold' for nothing. If this is a promotional offer, we may want to remove these items from the dataset, or denote them differently. 
+What duration did these sales take place over? Is this a random sampling, or is it a full year? Only 1,000 sales in a year? That seems a bit odd. We also have multiple accounts of what is otherwise the same sale; Dirk T Stringer selling 780 units of 'Mountain-100 Black, 38' 20 different times in a year? Does this make sense? Or is this a case of single transactions making it through our system as different Sales ID, in which case, our dataset is suspect. 
+Michael O'Leary has a Middle Initial of " ' ", an actual apostrophe is not a common middle Initial. Including Customer Name alongside ID would help with data visualization and comprehension. 
+
--- queries.sql
+++ queries.sql
@@ -1,35 +1,76 @@
 -- Problem 1 & 2
 -- 1. Using JOINs in a single query, combine data from all three tables (employees, products, sales) to view all sales with complete employee and product information in one table.
-select distinct * from employees JOIN sales on EmployeeID 
-
-;
+SELECT SalesID, SalesPersonID, CustomerID, Quantity, FirstName, MiddleInitial, LastName, name, price FROM sales JOIN employees as e on e.EmployeeID = sales.SalesPersonID JOIN products as p on p.ProductID = sales.ProductID;
 -- 2a. Create a View for the query you made in Problem 1 named "all_sales"
 -- NOTE: You'll want to remove any duplicate columns to clean up your view!
-CREATE VIEW all_sales as select distinct * from employees JOIN sales on EmployeeID 
-;
+CREATE VIEW all_sales as SELECT SalesID, SalesPersonID, CustomerID, Quantity, FirstName, MiddleInitial, LastName, name,(quantity * price) as total_sale, price FROM sales JOIN employees as e on e.EmployeeID = sales.SalesPersonID JOIN products as p on p.ProductID = sales.ProductID;
 
 -- 2b. Test your View by selecting all rows and columns from the View
 
 -- Problem 3
 -- Find the average sale amount for each sales person
+-- Below is not formatted the way that CodeDevCamp encourages, but is formatted in a way that I can read without visual tinnitus.
+Select 
+	SalespersonID, FirstName, MiddleInitial, LastName,	
+    avg(total_sale)
+    from all_sales
+    group by SalespersonID;
 
 -- Problem 4
 -- Find the top three sales persons by total sales
-
+Select distinct
+	SalespersonID, FirstName, MiddleInitial, LastName, (quantity * price) as Total_Sales
+    from all_sales
+    order by (quantity * price) desc
+    limit 3;
+    
 -- Problem 5
 -- Find the product that has the highest price
-
+Select distinct
+	name, Price 
+    from all_sales
+	order by Price desc
+    limit 1;
 -- Problem 6
 -- Find the product that was sold the most times
-
+Select 
+	name, Price, SUM(quantity) as TotalQuantity, (sum(quantity) * price) as TotalSales
+	from all_sales
+    group by name 
+    order by TotalQuantity desc
+	limit 1;
+    
 -- Problem 7
+Select *
+	from all_sales
+	where price > (select avg(price) from all_sales)
+	order by price desc;
 -- Using a subquery, find all products that have a price higher than the average price for all products
 
 -- Problem 8
 -- Find the customer who spent the most money in purchased products
+Select 
+	CustomerID, SUM(total_sale) as TotalSum
+	from all_sales
+    group by CustomerID  
+    order by TotalSum desc
+	limit 1;
 
 -- Problem 9
 -- Find the total number of sales for each sales person
+Select 
+	SalespersonID, FirstName, MiddleInitial, LastName, SUM(total_sale) as TotalSum
+	from all_sales
+    group by SalesPersonID  
+    order by TotalSum desc
+	;
+
 
 -- Problem 10
 -- Find the sales person who sold the most to the customer you found in Problem 8
+Select 
+	CustomerID, FirstName, MiddleInitial, LastName, SUM(total_sale) as TotalSum
+	from all_sales
+	Where CustomerID = 18723
+    group by SalesPersonID  
+    order by TotalSum desc;
